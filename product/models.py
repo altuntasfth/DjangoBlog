5d7cd389d49df8747278c0178f4ac8787b60a1ee
Fatih Altuntas <fatihaltuntas44@gmail.com>
2020-04-04 00:07:14 +0300

    Abouts, Refenrences

--- product/models.py
+++ product/models.py
@@ -1,3 +1,4 @@
+from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
 
 # Create your models here.
@@ -35,7 +36,7 @@ class Product(models.Model):
     image = models.ImageField(blank=True, upload_to='images/')
     price = models.FloatField()
     amount = models.IntegerField()
-    detail = models.TextField()
+    detail = RichTextUploadingField()
     status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
